8135017eb5f5c5801c04bd0872429f65c4a2e639
tim-parisi <tparisi@nd.edu>
2024-05-23 23:46:51 -0700

    Adding CSS Style Sheet

--- server.py
+++ server.py
@@ -10,19 +10,29 @@ PORT = 9999
 
 # Handlers
 
-class TestServer(tornado.web.RequestHandler):
+class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        
-        self.render('homepage.html')
+        self.render('index.html')
+class TestProjectHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('testproject.html')
+
 # Main Execution
 
 def main():
     application = tornado.web.Application([
-        (r'/', TestServer)
+        (r'/', IndexHandler),
+        (r'/index.html', IndexHandler),
+        (r'/testproject.html', TestProjectHandler),
+        (r"/static/(.*)", tornado.web.StaticFileHandler, {
+        "photo_path": "./photos" ,
+        "template_path": "./templates"
+        }),
+
     ])
 
     application.listen(PORT)
-    
+    print(f'Listening on port {PORT}')
     tornado.options.parse_command_line()
 
     tornado.ioloop.IOLoop.current().start()
